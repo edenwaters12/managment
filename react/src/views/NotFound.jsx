1f0762c9a9b2a420d03ffc7d28da5c6bb2d6c5ea
the <thedhruvish@gmail.com>
2024-10-20 11:18:52 +0530

    404 to logout

--- react/src/views/NotFound.jsx
+++ react/src/views/NotFound.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from "@/components/ui/Button.jsx";
+import axiosClient from "../axios-client.js";
+import { useStateContext } from '@/context/ContextProvider.jsx';
 
 const NotFoundPage = () => {
+  const { token, setUser, setToken, notification,setNotification } = useStateContext();
+
+  useEffect(() => {
+    if (!token) {
+      return
+    };
+
+    axiosClient.post('/logout')
+      .then(() => {
+        setUser({});
+        setToken(null);
+        setNotification('Logout............')
+      });
+  }, [token]);
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
       <div className="text-center">
@@ -9,10 +25,15 @@ const NotFoundPage = () => {
         <p className="mt-4 text-lg text-gray-700 dark:text-gray-300">
           Oops! The page you’re looking for doesn’t exist.
         </p>
-        <Button className="mt-6" onClick={() => window.location.href = '/'}>
-          Go Back Home
+        <Button className="mt-6" onClick={() => window.location.href = '/login'}>
+          Go Back Login
         </Button>
       </div>
+      {notification &&
+        <div className="fixed bottom-4 right-4 p-4 bg-gray-800 text-white rounded-lg shadow-lg">
+          {notification}
+        </div>
+      }
     </div>
   );
 };
