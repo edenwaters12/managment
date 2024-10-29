aac0b30173f33f4191c45ad37a12d62b841af79b
the <thedhruvish@gmail.com>
2024-10-29 22:33:46 +0530

    Default Layout desgin for the moblie and desktop

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,4 +1,4 @@
-import { Link, Navigate, Outlet } from "react-router-dom";
+import { NavLink, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../context/ContextProvider";
 import axiosClient from "../axios-client.js";
 import { useEffect, useState } from "react";
@@ -41,48 +41,104 @@ export default function DefaultLayout() {
     setMenuOpen(!menuOpen);
   };
 
+  const handleLinkClick = () => {
+    if (window.innerWidth < 768) {
+      setMenuOpen(false); // Close the menu on mobile view
+    }
+  };
+
   return (
     <>
       {!token ? (
         <Navigate to="/login" />
       ) : (
-        <div id="defaultLayout" className="flex min-h-screen relative z-1">
-          {/* Sidebar */}
+        <div
+          id="defaultLayout"
+          className="flex min-h-screen flex-col sm:flex-row "
+        >
           <aside
-            className={`w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform ${
-              menuOpen ? "translate-x-0" : "-translate-x-full"
-            } transition-transform duration-200 ease-in-out sm:relative sm:translate-x-0 sm:block flex flex-col  z-20`}
+            className={` sm:bg-slate-950 sm:bg-opacity-100 transform transition-transform duration-300 z-20 
+              ${menuOpen ? "translate-x-0" : "-translate-x-full"} 
+              sm:translate-x-0 sm:relative sm:w-64 w-full sm:flex flex-col py-6 px-4 fixed h-full`}
           >
-            <div className="flex items-center justify-between">
-              <h1 className="text-2xl font-bold">Dashboard</h1>
+            <div className="flex items-center justify-between mb-6">
+              <h1 className="text-2xl font-bold">MY SITE</h1>
               <button className="sm:hidden" onClick={toggleMenu}>
                 <X className="h-6 w-6" />
               </button>
             </div>
             <nav className="flex flex-col space-y-4 flex-grow items-center">
-              <Link to="/dashboard" className="hover:underline">
+              <NavLink
+                to="/dashboard"
+                onClick={handleLinkClick}
+                className={({ isActive }) =>
+                  `hover:underline  ${
+                    isActive ? "text-blue-500 font-semibold" : ""
+                  }`
+                }
+              >
                 Dashboard
-              </Link>
-              <Link to="/row" className="hover:underline">
+              </NavLink>
+              <NavLink
+                to="/row"
+                onClick={handleLinkClick}
+                className={({ isActive }) =>
+                  `hover:underline ${
+                    isActive ? "text-blue-500 font-semibold" : ""
+                  }`
+                }
+              >
                 Row Items
-              </Link>
+              </NavLink>
               {(user.role === "owner" || user.role === "admin") && (
                 <>
-                  <Link to="/science" className="hover:underline">
+                  <NavLink
+                    to="/science"
+                    onClick={handleLinkClick}
+                    className={({ isActive }) =>
+                      `hover:underline ${
+                        isActive ? "text-blue-500 font-semibold" : ""
+                      }`
+                    }
+                  >
                     Data Science Lecturers
-                  </Link>
-                  <Link to="/log" className="hover:underline">
+                  </NavLink>
+                  <NavLink
+                    to="/log"
+                    onClick={handleLinkClick}
+                    className={({ isActive }) =>
+                      `hover:underline ${
+                        isActive ? "text-blue-500 font-semibold" : ""
+                      }`
+                    }
+                  >
                     Log
-                  </Link>
-                  <Link to="/money" className="hover:underline">
+                  </NavLink>
+                  <NavLink
+                    to="/money"
+                    onClick={handleLinkClick}
+                    className={({ isActive }) =>
+                      `hover:underline ${
+                        isActive ? "text-blue-500 font-semibold" : ""
+                      }`
+                    }
+                  >
                     Money
-                  </Link>
-                  <Link to="/work" className="hover:underline">
+                  </NavLink>
+                  <NavLink
+                    to="/work"
+                    onClick={handleLinkClick}
+                    className={({ isActive }) =>
+                      `hover:underline ${
+                        isActive ? "text-blue-500 font-semibold" : ""
+                      }`
+                    }
+                  >
                     Work
-                  </Link>
+                  </NavLink>
                 </>
               )}
-              <div className="flex flex-col items-start mt-auto space-y-4">
+              <div className="mt-auto space-y-4">
                 <div className="flex items-center space-x-2">
                   <DropdownMenu>
                     <DropdownMenuTrigger className="flex items-center p-2 rounded-md">
@@ -104,15 +160,6 @@ export default function DefaultLayout() {
                   </DropdownMenu>
                 </div>
                 <span>{user.name}</span>
-                <div className="items-end">
-                  <LogOut
-                    size={33}
-                    color="#df2626"
-                    strokeWidth={2.5}
-                    className="cursor-pointer hover:text-red-800"
-                    onClick={onLogout}
-                  />
-                </div>
               </div>
             </nav>
           </aside>
@@ -120,14 +167,14 @@ export default function DefaultLayout() {
           {/* Backdrop for mobile menu */}
           {menuOpen && (
             <div
-              className="fixed inset-0 bg-black opacity-50 "
-              onClick={toggleMenu} // Close the menu when clicking outside
+              className="fixed inset-0 bg-black opacity-50 sm:hidden h-full"
+              onClick={toggleMenu}
             />
           )}
 
           {/* Main Content */}
           <div className="flex-1 p-4 bg-gray-100 dark:bg-gray-900">
-            <header className="flex justify-between items-center">
+            <header className="flex justify-between items-center mb-4">
               <button className="sm:hidden p-2" onClick={toggleMenu}>
                 {menuOpen ? (
                   <X className="h-6 w-6" />
@@ -135,9 +182,20 @@ export default function DefaultLayout() {
                   <Menu className="h-6 w-6" />
                 )}
               </button>
+              <div className="flex-grow flex justify-end mr-6">
+                {" "}
+                {/* New wrapper for alignment */}
+                <LogOut
+                  size={33}
+                  color="#df2626"
+                  strokeWidth={2.5}
+                  className="cursor-pointer hover:text-red-800"
+                  onClick={onLogout}
+                />
+              </div>
             </header>
 
-            <main className="mt-4">
+            <main>
               <Outlet />
             </main>
 
