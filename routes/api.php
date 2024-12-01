12e9418e38b4c04d8dfc566ae9688b87f4eb44f8
the <thedhruvish@gmail.com>
2024-12-01 22:20:53 +0530

    fix

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,7 @@ Route::middleware(['auth:sanctum', 'auto.logout'])->group(function () {
     Route::delete('/row/{id}', [RowItemController::class, 'destroy']);
 
     // API route for downloading a file
-    Route::get('/row/{id}/download/{fileName}', [RowItemController::class, 'download']);
+    Route::get('row/{id}/download/{fileName}', [RowItemController::class, 'download']);
     Route::get('/row/{id}/download', [CdmiDataController::class, 'downloadZip']);
     
     Route::get('/log', [IpInfoController::class, 'index']);
